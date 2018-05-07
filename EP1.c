78da817261b12e59e97d667053ac617986283c53
gabrieldacunha <gdcunha.r@gmail.com>
2018-05-06 23:50:48 -0300

    Limpeza de coisas antigas; pasta de dados fornecidos

---
+++ EP1.c
@@ -0,0 +1,20 @@
+/* EP1 - Metodos Numericos - MAP3121
+ *
+ * Daniel Lavedonio de Lima   - No USP: 8992882 - Turma 2
+ * Gabriel da Cunha Rodrigues - No USP: 8992930 - Turma 2
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Declaracao de funcoes >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+int main() {
+    /* Declaracao de variaveis */
+
+
+    return 0;
+}
